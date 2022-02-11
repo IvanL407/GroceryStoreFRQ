37b32c4238ec251f0de577451dbd00b73d564626
BT_1E10_14 <example@bths.edu>
2022-02-11 13:55:51 -0500

    finished grocerystore

--- src/GroceryStore.java
+++ src/GroceryStore.java
@@ -17,7 +17,15 @@ public class GroceryStore
      *  Precondition: min > 0 */
     public ArrayList<Integer> getReorderList(int min)
     {
-        /* to be implemented in part (a) */
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < productsStocked.length; i++)
+        {
+            if (productsStocked[i].getQuantity() <= min)
+            {
+                list.add(i);
+            }
+        }
+        return list;
     }
 
     /** Returns true if all products named in shoppingList are available for purchase
@@ -27,6 +35,32 @@ public class GroceryStore
      */
     public boolean checkAvailability(ArrayList<String> shoppingList)
     {
-        /* to be implemented in part (b) */
+        for (String item : shoppingList)
+        {
+            for (Product product : productsStocked)
+            {
+                if (product.getName().equals(item))
+                {
+                    if (product.getQuantity() == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public ArrayList<Product> oversized(double weight)
+    {
+        ArrayList<Product> list = new ArrayList<>();
+        for (Product item : productsStocked)
+        {
+            if (item.getWeight() > weight)
+            {
+                list.add(item);
+            }
+        }
+        return list;
     }
 }
